0fa04d96494b5e602031edc7bbebade3e583f295
Christian <chrisdaq@gmail.com>
2025-09-06 16:28:23 -0400

    Feat: Add DEAD_CODE rule set to Rector

--- rector.php
+++ rector.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\ValueObject\PhpVersion;
 
-return static function (RectorConfig $rectorConfig):
-{
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/.php-cs-fixer.dist.php',
         __DIR__ . '/src',
         __DIR__ . '/tests',
+    ]);
+
+    $rectorConfig->sets([
+        SetList::DEAD_CODE,
     ]);
 
     $rectorConfig->phpVersion(PhpVersion::PHP_83);
